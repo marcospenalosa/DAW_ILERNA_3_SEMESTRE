f7233fdc6133da4bdd2ad1709a4ba0a6a5885b81
MurquisDev <murquis@sobremesa>
2023-10-26 23:12:35 +0200

    M06-UF1 Añadidos los ejemplos 1 y 2

---
+++ M06_DEC/UF1/ejemplo1.js
@@ -0,0 +1,31 @@
+var a = "20"
+var b = 20.0
+
+console.log("Condicionales: ")
+
+if(a == b){
+    console.log("Son iguales")
+}
+if(a === b){
+    console.log("Son iguales y del mismo tipo de dato")
+}else{
+    console.log("Son iguales pero de otro tipo")
+}
+
+console.log("Operaciones del mismo tipo")
+var c = 2
+var suma = c + 2
+console.log(typeof(suma)) // integrer
+console.log(suma) // 4
+
+
+console.log("Operaciones distinto tipo:")
+var suma = b + "patitos"
+console.log(typeof(suma)) // string
+console.log(a+b) // 2020
+
+console.log("Bucle FOR")
+for(var i = 0; i < 10; i++){
+    a = a + i
+    console.log(a) //200; 20001; ... ; 200123456789
+}
